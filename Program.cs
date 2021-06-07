34101bb7189cd39b189a1a3e5c7b7bd5afe4edd2
Lucas <lucask.silvamendes@gmail.com>
2021-06-07 16:39:31 -0300

    classe e interface marca criadas

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projeto_Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
