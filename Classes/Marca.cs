88152943195bba6498c1119ea8746eee77ef8d92
Lucas <lucask.silvamendes@gmail.com>
2021-06-08 16:38:50 -0300

    marca atualizada

--- Classes/Marca.cs
+++ Classes/Marca.cs
@@ -12,14 +12,25 @@ namespace Projeto_Produtos.Classes
 
         List<Marca> ListaMarcas = new List<Marca>();
 
-        public string Cadastrar(Marca novaMarca)
+        public Marca()
+        {
+
+        }
+        public Marca(string _nomeMarca)
+        {
+            NomeMarca = _nomeMarca;
+            DataCadastro = DateTime.Now;
+
+        }
+
+        public string Cadastrar(Marca marca)
         {
             Console.WriteLine("Digite o código do produto");
-            novaMarca.Codigo = int.Parse(Console.ReadLine());
+            marca.Codigo = int.Parse(Console.ReadLine());
              Console.WriteLine("Digite o nome do produto");
-            novaMarca.NomeMarca = Console.ReadLine();
+            marca.NomeMarca = Console.ReadLine();
 
-            ListaMarcas.Add(novaMarca);
+            ListaMarcas.Add(marca);
             return "Marca cadastrada com sucesso!";
         }
 
@@ -28,9 +39,9 @@ namespace Projeto_Produtos.Classes
             return ListaMarcas;
         }
 
-        public string Deletar(Marca novaMarca)
+        public string Deletar(Marca marca)
         {
-            ListaMarcas.Remove(novaMarca);
+            ListaMarcas.RemoveAll(x => x.NomeMarca == marca.NomeMarca);
             return "Marca removida com sucesso!";
         }
 
