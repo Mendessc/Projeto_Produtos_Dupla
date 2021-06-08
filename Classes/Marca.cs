220ebf9aac726dfa9966e75a658438a3353ec313
Lucas <lucask.silvamendes@gmail.com>
2021-06-08 15:01:15 -0300

    private colocado

--- Classes/Marca.cs
+++ Classes/Marca.cs
@@ -6,9 +6,9 @@ namespace Projeto_Produtos.Classes
 {
     public class Marca : IMarca
     {
-        public int Codigo { get; set; }
-        public string NomeMarca { get; set; }
-        public DateTime DataCadastro { get; set; }
+        private int Codigo { get; set; }
+        private string NomeMarca { get; set; }
+        private DateTime DataCadastro { get; set; }
 
         List<Marca> ListaMarcas = new List<Marca>();
 
