379641b8fd94b75907df2a5d230d1de9e9e1f66e
Lucas <lucask.silvamendes@gmail.com>
2021-06-08 16:45:02 -0300

    produtos atualizados

--- Classes/Produto.cs
+++ Classes/Produto.cs
@@ -7,18 +7,32 @@ namespace Projeto_Produtos.Classes
 {
     public class Produto : IProduto
     {
-        private int CodigoProduto { get; set; }
+        private int Codigo { get; set; }
         private string NomeProduto { get; set; }
-        private DateTime DataCadastroP { get; set; }
+        private DateTime DataCadastro { get; set; }
         private float Preco { get; set; }
         private Marca marca { get; set; }
         private Usuario usuarios { get; set; }
-         List<Produto> ListaProdutos = new List<Produto>();
+        private List<Produto> ListaDeProdutos { get; set; }
+        List<Produto> ListaProdutos = new List<Produto>();
+        public Produto()
+        {
 
-         public string Cadastrar(Produto produto)
+        }
+        public Produto(int _codigo, string _nomeProduto, float _preco, Marca _marca, Usuario _usuarios)
+        {
+            this.Codigo = _codigo;
+            this.NomeProduto = _nomeProduto;
+            this.Preco = _preco;
+            this.DataCadastro = DateTime.Now;
+            this.marca = _marca;
+            this.usuarios = _usuarios;
+            this.ListaDeProdutos = ListaProdutos;
+        }
+        public string Cadastrar(Produto produto)
         {
             ListaProdutos.Add(produto);
-            return "Produto cadastrado com sucesso!";
+            return "Produto adicionado com sucesso!";
         }
 
         public List<Produto> Listar()
@@ -28,7 +42,7 @@ namespace Projeto_Produtos.Classes
 
         public string Deletar(Produto produto)
         {
-            ListaProdutos.Remove(produto);
+            ListaProdutos.RemoveAll(x => x.NomeProduto == produto.NomeProduto);
             return "Produto removido com sucesso!";
         }
     }
